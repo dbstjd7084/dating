f6ca6248ac697f5a9cacf831b2ee27a120bf55ec
dbstjd7084 <gmdqn96@gmail.com>
2025-01-19 23:09:20 +0900

    feat: 마이페이지 생성

---
+++ app/src/main/java/com/dbsthd2459/datingapp/setting/MyPageActivity.kt
@@ -0,0 +1,82 @@
+package com.dbsthd2459.datingapp.setting
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.dbsthd2459.datingapp.R
+import com.dbsthd2459.datingapp.auth.UserDataModel
+import com.dbsthd2459.datingapp.utils.FirebaseAuthUtils
+import com.dbsthd2459.datingapp.utils.FirebaseRef
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class MyPageActivity : AppCompatActivity() {
+
+    private val TAG = "MyPageActivity"
+
+    private val uid = FirebaseAuthUtils.getUid()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun getMyData() {
+
+        val myImage = findViewById<ImageView>(R.id.myImage)
+
+        val myUid = findViewById<TextView>(R.id.myUid)
+        val myNickname = findViewById<TextView>(R.id.myNickname)
+        val myAge = findViewById<TextView>(R.id.myAge)
+        val myCity = findViewById<TextView>(R.id.myCity)
+        val myGender = findViewById<TextView>(R.id.myGender)
+
+        val postListener = object : ValueEventListener {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                val data = dataSnapshot.getValue(UserDataModel::class.java)
+
+                myUid.text = data!!.uid
+                myNickname.text = data!!.nickname
+                myAge.text = data!!.age
+                myCity.text = data!!.city
+                myGender.text = data!!.gender
+
+                val storageRef = Firebase.storage.reference.child(data.uid + ".png")
+
+                storageRef.downloadUrl.addOnCompleteListener({ task ->
+                    if (task.isSuccessful) {
+                        Glide.with(baseContext)
+                            .load(task.result)
+                            .into(myImage)
+                    }
+                })
+
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FirebaseRef.userInfoRef.child(uid).addValueEventListener(postListener)
+    }
+}
---
+++ app/src/main/java/com/dbsthd2459/datingapp/setting/SettingActivity.kt
@@ -0,0 +1,39 @@
+package com.dbsthd2459.datingapp.setting
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dbsthd2459.datingapp.R
+import com.dbsthd2459.datingapp.auth.IntroActivity
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        val mybtn = findViewById<Button>(R.id.myPageBtn)
+        mybtn.setOnClickListener {
+
+            val intent = Intent(this, MyPageActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener {
+
+            val auth = Firebase.auth
+            auth.signOut()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
