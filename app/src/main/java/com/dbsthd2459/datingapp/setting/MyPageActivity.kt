47709e64208071c353736ecca808c904ab0b8b48
dbstjd7084 <gmdqn96@gmail.com>
2025-02-12 16:48:53 +0900

    chore: 색 추가
    - 배경 색 구분
    - 회색 및 보조색 추가

--- app/src/main/java/com/dbsthd2459/datingapp/setting/MyPageActivity.kt
+++ app/src/main/java/com/dbsthd2459/datingapp/setting/MyPageActivity.kt
@@ -1,16 +1,23 @@
 package com.dbsthd2459.datingapp.setting
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.dbsthd2459.datingapp.MainActivity
 import com.dbsthd2459.datingapp.R
+import com.dbsthd2459.datingapp.auth.IntroActivity
 import com.dbsthd2459.datingapp.auth.UserDataModel
+import com.dbsthd2459.datingapp.message.MyLikeListActivity
 import com.dbsthd2459.datingapp.utils.FirebaseAuthUtils
 import com.dbsthd2459.datingapp.utils.FirebaseRef
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -28,29 +35,67 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
         getMyData()
+
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+        logoutBtn.setOnClickListener {
+
+            val auth = Firebase.auth
+            auth.signOut()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        // 네비게이션 바 클릭 시 이벤트 설정
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_profile -> {
+                    val intent = Intent(this, MyPageActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                R.id.nav_chat -> {
+                    val intent = Intent(this, MyLikeListActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                R.id.nav_matching -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun getMyData() {
 
         val myImage = findViewById<ImageView>(R.id.myImage)
 
-        val myUid = findViewById<TextView>(R.id.myUid)
+        val myEmail = findViewById<TextView>(R.id.myEmail)
         val myNickname = findViewById<TextView>(R.id.myNickname)
         val myAge = findViewById<TextView>(R.id.myAge)
         val myCity = findViewById<TextView>(R.id.myCity)
         val myGender = findViewById<TextView>(R.id.myGender)
 
         val postListener = object : ValueEventListener {
-            @SuppressLint("NotifyDataSetChanged")
+            @SuppressLint("NotifyDataSetChanged", "SetTextI18n")
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
                 val data = dataSnapshot.getValue(UserDataModel::class.java)
 
-                myUid.text = data!!.uid
-                myNickname.text = data.nickname
-                myAge.text = data.age
+                myEmail.text = FirebaseAuthUtils.getEmail()
+                myNickname.text = data!!.nickname
+                myAge.text = data.age + "세"
+                if (data.gender == "M") myGender.text = "남성"
+                else myGender.text = "여성"
                 myCity.text = data.city
-                myGender.text = data.gender
 
                 val storageRef = Firebase.storage.reference.child(data.uid + ".png")
 
@@ -58,6 +103,7 @@ class MyPageActivity : AppCompatActivity() {
                     if (task.isSuccessful) {
                         Glide.with(baseContext)
                             .load(task.result)
+                            .circleCrop()
                             .into(myImage)
                     }
                 })
